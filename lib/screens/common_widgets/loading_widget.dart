7d8099266c3d3c133e528908565d541fe4f67183
snehaldigitalflake <snehal@digitalflake.com>
2023-06-08 13:00:33 +0530

    added loader

--- lib/screens/common_widgets/loading_widget.dart
+++ lib/screens/common_widgets/loading_widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:lottie/lottie.dart';
-import 'package:shimmer/shimmer.dart';
 import 'package:test_app/utils/app_images.dart';
 
 class LoadingWidget extends StatelessWidget {
@@ -10,11 +9,7 @@ class LoadingWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Shimmer.fromColors(
-        baseColor: Colors.grey.withOpacity(0.4),
-        highlightColor: Colors.white.withOpacity(0.5),
-        child: Lottie.asset(AppImages.loader, height: 200.h),
-      ),
+      child: Lottie.asset(AppImages.loader, height: 200.h),
     );
   }
 }
